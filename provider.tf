ce39aa9b500db6576959cf26f37a0654f6a62467
I-Ray <chinnivenkatapavan@gmail.com>
2025-01-23 18:02:45 +0530

    updated terraform

--- provider.tf
+++ provider.tf
@@ -1,10 +1,8 @@
-
-terraform {
-  backend "azurerm" {}
-}
-
 provider "azurerm" {
   features {}
   subscription_id = "7b6c642c-6e46-418f-b715-e01b2f871413"
 }
 
+terraform {
+  backend "azurerm" {}
+}
