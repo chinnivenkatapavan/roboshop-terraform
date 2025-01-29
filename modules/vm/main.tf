6f8a9bdfc539839524b39382e73325f25f4bb494
chinni venkatapavan <chinnivenkatapavan@gmil.com>
2025-01-29 21:58:38 +0530

    Added New concept in my list

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -112,7 +112,7 @@ resource "null_resource" "ansible" {
 
     inline = [
       "sudo dnf install python3.12-pip -y",
-      "sudo pip3.12 install ansible",
+      "sudo pip3.12 install ansible hvac",
       "ansible-pull -i localhost, -U https://github.com/chinnivenkatapavan/roboshop-ansible.git roboshop.yml -e app_name=${var.component} -e ENV=${var.env} -e token=${var.token}"
     ]
   }
