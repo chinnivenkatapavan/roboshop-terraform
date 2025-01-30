fc1c335e7210b33d6927a5c1a2e3ab762eb5374e
chinni venkatapavan <chinnivenkatapavan@gmil.com>
2025-01-30 07:12:36 +0530

    Added New concept in my list

--- main.tf
+++ main.tf
@@ -8,11 +8,15 @@
 #   container   = each.value["container"]
 # }
 
-module "component"{
-    for_each = var.component
-    source = "./modules/vm"
-    component = each.value["name"]
-    env       = var.env
-    token = var.token
-    container = each.value["container"]
+# module "component"{
+#     for_each = var.component
+#     source = "./modules/vm"
+#     component = each.value["name"]
+#     env       = var.env
+#     token = var.token
+#     container = each.value["container"]
+# }
+
+module "aks" {
+    source = "./modules/aks"
 }
