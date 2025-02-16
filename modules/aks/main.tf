a10e1f4d189e9ba2f44835e6b92571e4491aa5eb
chinni venkatapavan <chinnivenkatapavan@gmil.com>
2025-02-16 13:45:47 +0530

    spec:   selector:     app: shipping is not in correct space

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -9,9 +9,9 @@ resource "azurerm_kubernetes_cluster" "main" {
     name       = "default"
     node_count = 1
     vm_size    = "Standard_D2_v2"
+    auto_scaling_enabled = true
     min_count = 1
     max_count = 10
-    auto_scaling_enabled = true
     #pod_subnet_id = "/subscriptions/b3406e49-0e16-456d-a3bb-c324f1dfb0d5/resourceGroups/project-setup-1/providers/Microsoft.Network/virtualNetworks/Project-Network/subnets/default"
     vnet_subnet_id = "/subscriptions/b3406e49-0e16-456d-a3bb-c324f1dfb0d5/resourceGroups/project-setup-1/providers/Microsoft.Network/virtualNetworks/Project-Network/subnets/default"
 
