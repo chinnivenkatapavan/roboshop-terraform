a17f8514ed656fc39deda1c12b36a6bf3ac34837
chinni venkatapavan <chinnivenkatapavan@gmil.com>
2025-02-19 05:49:15 +0530

    spec:   selector:     app: shipping is not in correct space

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -23,7 +23,7 @@ resource "azurerm_kubernetes_cluster" "main" {
 
   network_profile {
     network_plugin = "azure"
-    service_cidr = "10.0.0.0/16"
+    service_cidr = "10.0.0.0/24"
     dns_service_ip = "10.0.0.10"
   }
 
