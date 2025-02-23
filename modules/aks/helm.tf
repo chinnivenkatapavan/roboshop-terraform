54b14c1435a2b0ff84cef7cbfdcee4cae097772c
chinni venkatapavan <chinnivenkatapavan@gmil.com>
2025-02-23 14:21:07 +0530

    corrected the vault ip address in cart

---
+++ modules/aks/helm.tf
@@ -0,0 +1,18 @@
+
+resource "null_resource" "kubeconfig" {
+  provisioner "local-exec" {
+    command = <<EOF
+az aks get-credentials --resource-group ${data.azurerm_resource_group.main.name} --name main --overwrite-existing
+kubectl apply -f /opt/vault-token.yml
+EOF
+  }
+
+}
+
+resource "helm_release" "external-secrets" {
+  depends_on = [null_resource.kubeconfig]
+  name       = "external-secrets"
+  repository = "https://charts.external-secrets.io"
+  chart      = "external-secrets"
+  namespace  = "kube-system"
+}
